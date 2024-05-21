df5185521f702f762b94517aa58115eb5ff2db98
yashjawale <yash25.j@gmail.com>
2024-05-21 23:44:09 +0530

    Add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yashjawale.github.io',
   base: '/playground/',
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), react()]
 });
