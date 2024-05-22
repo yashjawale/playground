12685f076dd2d30ff69f4c79e555a181884910fa
yashjawale <yash25.j@gmail.com>
2024-05-22 14:01:17 +0530

    Enable analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yashjawale.github.io',
   base: '/playground/',
-  integrations: [tailwind(), icon(), react()]
+  integrations: [tailwind(), icon(), react(), partytown()]
 });
