76252d0bffac7cc51a977c7478a5dfd8352d5ace
yashjawale <yash25.j@gmail.com>
2025-08-17 14:21:56 +0530

    refactor: update font and enhance dark mode styles
    - Replaced Manrope font with Inter Variable font in package.json and relevant components.
    - Improved Navbar and Layout components to support dark mode with appropriate background and text colors.
    - Enhanced ThemeSelector component with dark mode support and updated styles.
    - Refactored global CSS to define new color variables for light and dark themes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
-
+import tailwindcss from "@tailwindcss/vite";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yashjawale.github.io',
   base: '/playground/',
-  integrations: [tailwind(), icon(), react(), partytown()]
+  integrations: [icon(), react(), partytown()],
+	vite: {
+    plugins: [tailwindcss()],
+  }
 });
