e71cfe4fbc5445f2881d96f60eff9bd627325a6f
yashjawale <yash25.j@gmail.com>
2026-08-16 12:29:45 +0530

    chore: fix formatting

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,9 +8,7 @@ export default defineConfig({
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 1 : 0,
-	reporter: process.env.CI
-		? [['github'], ['html', { open: 'never' }]]
-		: 'list',
+	reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : 'list',
 	use: {
 		baseURL: BASE_URL,
 		viewport: { width: 1280, height: 720 },
